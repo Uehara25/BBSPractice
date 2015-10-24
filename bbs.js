23a050e3fa190fae6f559c12d99074d251e19ccf
Uehara25 <akiya.noface@gmail.com>
2015-10-24 16:20:39 +0900

    骨組みの形を変更

--- bbs.js
+++ bbs.js
@@ -10,19 +10,18 @@ var ADDRESS = 'localhost';
 var PORT = '5000';
 
 function onRequest(request, response){
-    if(response.url == '/'){
-        // 普通にサイトに来た時の処理
+    if(response.url != '/'){
+        if(request.method != 'POST'){
+            // GET リクエストが来た時の処理。
+            // 他のリクエストが来たときにどうしたらいいのか分からないので、とりあえず他もこれで処理します。
+        }
+    
+        if(request.method == 'POST'){
+            // POST リクエストが来た時の処理。テキストフォームと名前入力欄からデータを受け取り、保存する処理を行い、/postedに移動させる。
+        }
+
     }else{
-        // 存在しない場所にアクセスした場合
-    }
-
-    if(request.method != 'POST'){
-        // GET リクエストが来た時の処理。
-        // 他のリクエストが来たときにどうしたらいいのか分からないので、とりあえず他もこれで処理します。
-    }
-
-    if(request.method == 'POST'){
-        // POST リクエストが来た時の処理。テキストフォームと名前入力欄からデータを受け取り、保存する処理を行い、/postedに移動させる。
+        
     }
 }
 
